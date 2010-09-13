b0819468f0fdb7a2235fa442467719262b6209d0
Josef Fruehwald <JoFrhwld@gmail.com>
2010-09-12 22:31:43 -0400

    worked until dictionary bug on remeasure.py

--- python/remeasure.py
+++ python/remeasure.py
@@ -1,13 +1,48 @@
 file = "/Users/joseffruehwald/Documents/Classes/Fall_10/misc/FAAV/extractFormants_modified/PH06-2-1-AB-Jean.formants"
 f = open(file)
-line = f.readline()
-line = f.readline()
-line = f.readline()
-line = f.readline().rstrip()
+l = f.readline()
+l = f.readline()
+l = f.readline()
+print("Reading file...")
+lines = f.readlines()
+f.close()
+print("File read")
+
+
 
 import ast
+import math
+import rpy2
 
-line2 = line.split("\t")
+vowels = {}
 
-poles = ast.literal_eval(line2[20])
-bandwidths = ast.literal_eval(line2[21])
+for line in lines:
+    line = line.rstrip()
+    line = line.split("\t")
+    F1 = float(line[4])
+    F2 = float(line[5])
+    B1 = float(line[7])
+    B2 = float(line[8])
+    Dur = math.log1p(float(line[13]))
+
+    print(line[0])
+    print([F1, F2, B1, B2, Dur])
+    print(line[0] in vowels)
+    if line[0] in vowels:
+        vowels[line[0]] = vowels[line[0]].append([F1, F2, B1, B2, Dur])
+    else:
+        vowels[line[0]] = [[F1, F2, B1, B2, Dur]]
+        
+
+    ## right now returns error
+    ## Traceback (most recent call last):
+      #File "/Users/joseffruehwald/Documents/Classes/Fall_10/FAAV/python/remeasure.py", line 32, in <module>
+    #vowels[line[0]] = vowels[line[0]].append([F1, F2, B1, B2, Dur])
+    #AttributeError: 'NoneType' object has no attribute 'append'
+    #
+    #For some reason, the first IY and AY have None for values.
+
+#poles = ast.literal_eval(line2[20])
+#bandwidths = ast.literal_eval(line2[21])
+
+
