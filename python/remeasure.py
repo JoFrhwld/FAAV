02bd7770680e2e0a18dded5255e86a5ee59df2ec
Josef Fruehwald <JoFrhwld@gmail.com>
2010-09-13 12:07:05 -0400

    remeasure.py fixed initial dictionary problem

--- python/remeasure.py
+++ python/remeasure.py
@@ -25,22 +25,14 @@ for line in lines:
     B2 = float(line[8])
     Dur = math.log1p(float(line[13]))
 
-    print(line[0])
-    print([F1, F2, B1, B2, Dur])
-    print(line[0] in vowels)
+    
     if line[0] in vowels:
-        vowels[line[0]] = vowels[line[0]].append([F1, F2, B1, B2, Dur])
+        vowels[line[0]].append([F1, F2, B1, B2, Dur])
     else:
         vowels[line[0]] = [[F1, F2, B1, B2, Dur]]
         
 
-    ## right now returns error
-    ## Traceback (most recent call last):
-      #File "/Users/joseffruehwald/Documents/Classes/Fall_10/FAAV/python/remeasure.py", line 32, in <module>
-    #vowels[line[0]] = vowels[line[0]].append([F1, F2, B1, B2, Dur])
-    #AttributeError: 'NoneType' object has no attribute 'append'
-    #
-    #For some reason, the first IY and AY have None for values.
+    
 
 #poles = ast.literal_eval(line2[20])
 #bandwidths = ast.literal_eval(line2[21])
