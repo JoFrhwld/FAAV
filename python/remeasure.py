b826e22a5fc61c628df779539bc70a9fd823e034
Josef Fruehwald <JoFrhwld@gmail.com>
2010-09-13 12:41:12 -0400

    remeasure.py computes means and covariances

--- python/remeasure.py
+++ python/remeasure.py
@@ -12,7 +12,7 @@ print("File read")
 
 import ast
 import math
-import rpy2
+import rpy2.robjects as robjects
 
 vowels = {}
 
@@ -30,9 +30,25 @@ for line in lines:
         vowels[line[0]].append([F1, F2, B1, B2, Dur])
     else:
         vowels[line[0]] = [[F1, F2, B1, B2, Dur]]
-        
 
+vowelMeans = {}
+vowelCovs = {}
+for vowel in vowels:
+    vF1 = robjects.FloatVector([F1 for [F1,F2,B1,B2,Dur] in vowels[vowel]])
+    vF2 = robjects.FloatVector([F2 for [F1,F2,B1,B2,Dur] in vowels[vowel]])
+    vB1 = robjects.FloatVector([B1 for [F1,F2,B1,B2,Dur] in vowels[vowel]])
+    vB2 = robjects.FloatVector([B2 for [F1,F2,B1,B2,Dur] in vowels[vowel]])
+    vDur = robjects.FloatVector([Dur for [F1,F2,B1,B2,Dur] in vowels[vowel]])
+
+    rcolMeans = robjects.r["colMeans"]
+    rcov = robjects.r["cov"]
+
+    measureMatrix = robjects.r["matrix"](vF1 + vF2 + vB1 + vB2 + vDur, ncol = 5)
     
+    vowelMeans[vowel] = rcolMeans(measureMatrix)
+    vowelCovs[vowel] = rcov(measureMatrix)
+
+        
 
 #poles = ast.literal_eval(line2[20])
 #bandwidths = ast.literal_eval(line2[21])
