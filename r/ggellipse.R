7d2fd0d4bf584abd329376fdda557bf47cda8483
Josef Fruehwald <JoFrhwld@gmail.com>
2011-08-15 13:01:52 -0400

    updated ggelipse

--- r/ggellipse.R
+++ r/ggellipse.R
@@ -1,24 +1,3 @@
-my.ellipse <- function(x, y, level = 0.75, segments = 51){
-	dfn <- 2
-    dfd <- length(x) - 1
-    if (dfd < 3){
-    	ellipse <- rbind(c(NA,NA))	
-    } else {
-        require(MASS)
-        v <- cov.trob(cbind(x, y))
-        shape <- v$cov
-        center <- v$center
-        radius <- sqrt(dfn * qf(level, dfn, dfd))
-        angles <- (0:segments) * 2 * pi/segments
-    	unit.circle <- cbind(cos(angles), sin(angles))
-	    ellipse <- t(center + radius * t(unit.circle %*% chol(shape)))
-    }
-    
-    ellipse <- as.data.frame(ellipse)
-    colnames(ellipse) <- c("x","y")
-    return(ellipse)
-}
-
 StatEllipse <- proto(Stat,
 	{
 		required_aes <- c("x", "y")
@@ -29,7 +8,24 @@ StatEllipse <- proto(Stat,
 			.super$calculate_groups(., data, scales,...)
 		}
 		calculate <- function(., data, scales, level = 0.75, segments = 51,...){
-			my.ellipse(data$x, data$y, level = level, segments = segments)	
+      dfn <- 2
+      dfd <- length(data$x) - 1
+      if (dfd < 3){
+      	ellipse <- rbind(c(NA,NA))	
+      } else {
+          require(MASS)
+          v <- cov.trob(cbind(data$x, data$y))
+          shape <- v$cov
+          center <- v$center
+          radius <- sqrt(dfn * qf(level, dfn, dfd))
+          angles <- (0:segments) * 2 * pi/segments
+          unit.circle <- cbind(cos(angles), sin(angles))
+          ellipse <- t(center + radius * t(unit.circle %*% chol(shape)))
+      }
+    
+      ellipse <- as.data.frame(ellipse)
+      colnames(ellipse) <- c("x","y")
+      return(ellipse)
 		}
 	}
 )
